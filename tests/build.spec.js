8345fc760eaa465915ac82b14ae65b24328bf574
Alex Neamtu <alexneamtu@gmail.com>
2016-11-26 12:38:30 +0200

    Remove tests, for now

--- tests/build.spec.js
+++ tests/build.spec.js
@@ -1,6 +1,6 @@
 var test = require('ava');
 var myModule = require('../dist/build');
 
-test('the log method', t => {
-    t.is(myModule.log('hi'), 'Logging the message : hi');
-});
+// test('the log method', t => {
+//     t.is(myModule.log('hi'), 'Logging the message : hi');
+// });
